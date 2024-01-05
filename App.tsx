3f4c925eda85869bb9440391ec88a56cb9a04eab
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2024-01-04 23:41:28 -0300

    criacao da box do pokemon

--- App.tsx
+++ App.tsx
@@ -1,118 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import { styles } from './src/styles/global.style';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
+  useColorScheme,
+  Image
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Pokédex</Text>
+      <ScrollView style={styles.pokemonList}>
+        <View style={styles.pokemon}>
+          <View >
+            <Text style={styles.pokemonNumber}>#001</Text>
+            <Text style={styles.pokemonName}>Bulbasaur</Text>
+            <View>
+              <Text style={styles.type}>Grass</Text>
+              <Text style={styles.type}>Poison</Text>
+            </View>
+
+          </View>
+          <Image source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png' }}
+            style={{ width: 180, height: 230, alignSelf: 'flex-end' }} />
+        </View>
+        <View style={styles.pokemon}>
+          <View >
+            <Text style={styles.pokemonNumber}>#001</Text>
+            <Text style={styles.pokemonName}>Bulbasaur</Text>
+            <View>
+              <Text style={styles.type}>Grass</Text>
+              <Text style={styles.type}>Poison</Text>
+            </View>
+
+          </View>
+          <Image source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png' }}
+            style={{ width: 180, height: 230, alignSelf: 'flex-end' }} />
+        </View>
+        <View style={styles.pokemon}>
+          <View >
+            <Text style={styles.pokemonNumber}>#001</Text>
+            <Text style={styles.pokemonName}>Bulbasaur</Text>
+            <View>
+              <Text style={styles.type}>Grass</Text>
+              <Text style={styles.type}>Poison</Text>
+            </View>
+
+          </View>
+          <Image source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png' }}
+            style={{ width: 180, height: 230, alignSelf: 'flex-end' }} />
+        </View>
+        <View style={styles.pokemon}>
+          <View >
+            <Text style={styles.pokemonNumber}>#001</Text>
+            <Text style={styles.pokemonName}>Bulbasaur</Text>
+            <View>
+              <Text style={styles.type}>Grass</Text>
+              <Text style={styles.type}>Poison</Text>
+            </View>
+
+          </View>
+          <Image source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png' }}
+            style={{ width: 180, height: 230, alignSelf: 'flex-end' }} />
+        </View>
+        <View style={styles.pokemon}>
+          <View >
+            <Text style={styles.pokemonNumber}>#001</Text>
+            <Text style={styles.pokemonName}>Bulbasaur</Text>
+            <View>
+              <Text style={styles.type}>Grass</Text>
+              <Text style={styles.type}>Poison</Text>
+            </View>
+
+          </View>
+          <Image source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png' }}
+            style={{ width: 180, height: 230, alignSelf: 'flex-end' }} />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
